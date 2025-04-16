940b771ad1bec85022a0a371c14c93dbb8b75b34
KCMbogo <kadilana04@gmail.com>
2025-04-16 17:13:09 +0300

    Mods

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('products/', views.product_list_view, name='product_list'),
     path('product/<str:pid>', views.single_product_view, name='product'),
+    
+    path('vendors/', views.vendor_list_view, name="vendor_list"),
+    path('vendor/<str:vid>', views.single_vendor_view, name='vendor')
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product, Category, ProductImages
+from .models import Product, Category, ProductImages, Vendor
 
 def index(request):
     categories = Category.objects.all()
@@ -13,8 +13,11 @@ def index(request):
 def product_list_view(request):
     # products = Product.objects.filter(product_status='published')
     products = Product.objects.all()
+    categories = Category.objects.all()
+
     context = {
         'products': products,
+        'categories': categories,
     }
     return render(request, 'core/products.html', context)
 
@@ -26,14 +29,39 @@ def single_product_view(request, pid):
         return render(request, 'core/404.html', {'product': product})
     
     product_images = ProductImages.objects.filter(product=product)
+    related_products = Product.objects.filter(vendor=product.vendor).exclude(pid=product.pid)[:4]
     
     context = {
         'product': product,
-        'product_images': product_images
+        'product_images': product_images,
+        'related_products': related_products,
     }
     
     return render(request, 'core/single_product.html', context)
     
-        
-        
+def vendor_list_view(request):
+    vendors = Vendor.objects.all()
+    count = vendors.count()
+    
+    context = {
+        'vendors': vendors,
+        'vendor_count':  count,
+    }    
+    
+    return render(request, 'core/vendors.html', context)    
 
+def single_vendor_view(request, vid):
+    try:
+        vendor = Vendor.objects.get(vid=vid)
+    except Vendor.DoesNotExist:
+        vendor = None
+        return render(request, 'core/404.html', {'vendor': vendor})
+    
+    products = Product.objects.filter(vendor=vendor)
+    
+    context = {
+        'products': products,
+        'vendor': vendor
+    }
+    
+    return render(request, 'core/single_vendor.html', context)
